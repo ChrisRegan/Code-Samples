38f6b8dbbb91a6fbbd5a3b9b23365b2b58dc2bc3
Chris R <chrisregan50@gmail.com>
2024-03-15 16:17:36 -0500

    Initial push of a match-3
    First pass at match 3 game.
    
    Currently in.  When 4 or more of the same color is connected delete the objects.
    
    move the player left or right at the bottom of the screen to select the bottom most block.
    
    Hit j to grab and place a block
    
    Have the blocks move from the top of the screen to the bottom of the screen.
    
    End the game when 8 lines of blocks are on the screen.
    
    ToDo:
    
    Make the blocks fall back to the next closest block.
    Add some sounds to the sample.
    Improve the graphics to look more complete
    Scoring system.

---
+++ Defold/Match-3/Assets/Tiles/tile.go
@@ -0,0 +1,25 @@
+embedded_components {
+  id: "tileSprite"
+  type: "sprite"
+  data: "tile_set: \"/Assets/Tiles/Tiles.atlas\"\n"
+  "default_animation: \"BlueTile\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  scale {
+    x: 0.5
+    y: 0.5
+    z: 1.0
+  }
+}
