1e70faa30293563439791b1fa06364714183b45e
Chris R <chrisregan50@gmail.com>
2024-03-18 17:05:14 -0500

    finished basic first pass.  Cant find any bugs at this point
    Finished first pass of the game.
    
    Features would like to add still.
          Difficulty progression
          score
          BG music?
          swapping blocks

--- Defold/Match-3/Assets/Tiles/tile.go
+++ Defold/Match-3/Assets/Tiles/tile.go
@@ -17,9 +17,24 @@ embedded_components {
     z: 0.0
     w: 1.0
   }
-  scale {
-    x: 0.5
-    y: 0.5
-    z: 1.0
+}
+embedded_components {
+  id: "collected"
+  type: "sprite"
+  data: "tile_set: \"/Assets/Tiles/Tiles.atlas\"\n"
+  "default_animation: \"collectedSprite\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.1
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
   }
 }
