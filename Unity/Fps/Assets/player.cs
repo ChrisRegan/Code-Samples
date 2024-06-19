d432456af3b02c1c7470d26622eb5e69ddd200cf
Chris R <chrisregan50@gmail.com>
2024-06-19 17:27:15 -0500

    Updated movement
    Spent to long trying to get animation for walk cycle right.  Will probably add the terrible walk animation in next submission to stop wasting time on it. To continue making the game.

--- Unity/Fps/Assets/player.cs
+++ Unity/Fps/Assets/player.cs
@@ -6,7 +6,8 @@ public class player : MonoBehaviour
 {
 
     Rigidbody m_Rigidbody;
-    public float m_Speed = 1f;
+    public float m_HoveringSpeed = 1f;
+    public float m_WalkingSpeed = 1f;
     public Vector3 m_jumpStrength = new Vector3(0, 50f, 0);
     int touchingGround = 0;
     bool onGround = false;
@@ -14,11 +15,18 @@ public class player : MonoBehaviour
     Vector2 rotation = new Vector2(0.0f,0.0f);
     public float mouseSensitivity = 1.0f;
     public float clampX = 30f;
+    public bool isHovering = false;
+    public float dragIncrease = 100;
+    private float drag;
+    public float LookUpCap = 0.0f;
+    public float LookDownCap = 0.0f;
+
 
     // Start is called before the first frame update
     void Start()
     {
         m_Rigidbody = GetComponent<Rigidbody>();
+        drag = m_Rigidbody.drag;
         playerCamera = gameObject.GetComponentInChildren<Camera>();
         playerCamera.transform.localRotation = Quaternion.Euler(rotation.x * mouseSensitivity, 0, 0);
     }
@@ -28,22 +36,42 @@ public class player : MonoBehaviour
 
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    void ToggleHoverMode()
+    {
+        isHovering = !isHovering;
+    }
+
+    void HoveringMode()
+    {
+        m_Rigidbody.AddForce(Input.GetAxis("Vertical") * transform.forward * m_HoveringSpeed);
+        m_Rigidbody.AddForce(Input.GetAxis("Horizontal") * transform.right * m_HoveringSpeed);
+    }
+
+    void WalkingMode()
     {
 
+        Vector3 forward = Input.GetAxis("Vertical") * transform.forward;
+        Vector3 side = Input.GetAxis("Horizontal") * transform.right;
+        Vector3 direction = (forward + side) * m_WalkingSpeed;
+        m_Rigidbody.MovePosition(transform.position + direction);
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {        
         // Movement
-        if (Input.GetAxis("Vertical") != 0.0f)
+        if (!isHovering)
         {
-            //Move the Rigidbody forwards constantly at speed you define (the blue arrow axis in Scene view)
-            m_Rigidbody.AddForce(Input.GetAxis("Vertical") * transform.forward * m_Speed);
+            m_Rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+            WalkingMode();
+        }
+        else
+        {
+            m_Rigidbody.constraints = RigidbodyConstraints.None;
+            HoveringMode();
         }
 
-        if (Input.GetAxis("Horizontal") != 0.0f)
-        {
-            //Move the Rigidbody forwards constantly at speed you define (the blue arrow axis in Scene view)
-            m_Rigidbody.AddForce(Input.GetAxis("Horizontal") * transform.right * m_Speed);
-        }
 
         if (onGround && Input.GetButton("Jump"))
         {
@@ -55,7 +83,7 @@ public class player : MonoBehaviour
 
         rotation.y += Input.GetAxis("Mouse X");
         rotation.x += -Input.GetAxis("Mouse Y");
-        rotation.x = Mathf.Clamp(rotation.x, -clampX, clampX);
+        rotation.x = Mathf.Clamp(rotation.x, LookDownCap, LookUpCap);
         transform.eulerAngles = new Vector2(0, rotation.y) * mouseSensitivity;
         playerCamera.transform.localRotation = Quaternion.Euler(rotation.x * mouseSensitivity, 0, 0);
 
